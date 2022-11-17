8b379c1137ae14d4738e0fc5f6c17e26e011e03a
sindwarf <sindorfsolutions@gmail.com>
2022-11-16 17:31:27 -0800

    end of day 1

--- server/schema.sql
+++ server/schema.sql
@@ -5,28 +5,31 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  id INTEGER(11) AUTO_INCREMENT PRIMARY KEY,
-  room_id INTEGER(11),
+  id INTEGER(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   text VARCHAR(1000),
-  username_id INTEGER(11)
-  -- FOREIGN KEY(username) REFERENCES (usernames.text),
-  -- FOREIGN KEY(room_id) REFERENCES (rooms(id))
+  room_id INTEGER(11),
+  username_id INTEGER(11),
+
+  FOREIGN KEY(username_id) REFERENCES (usernames(id)),
+  FOREIGN KEY(room_id) REFERENCES (rooms(id))
   /* Describe your table here.
   message id
   room_name
   username
   text
+      INDEX par_ind (parent_id),
+      FOREIGN KEY (parent_id) REFERENCES parent(id)
   */
 );
 
 CREATE TABLE rooms (
-  id INTEGER(11) AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   text VARCHAR(1000)
 );
 
 
 CREATE TABLE usernames (
-  id INTEGER(11) AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   text VARCHAR(1000)
 );
 /* Create other tables and define schemas for them here! */
