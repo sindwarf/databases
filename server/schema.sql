391796e26939f5197709e0f1344cb15642481a5e
sindwarf <sindorfsolutions@gmail.com>
2022-11-16 17:16:52 -0800

    wrote schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -1,11 +1,34 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INTEGER(11) AUTO_INCREMENT PRIMARY KEY,
+  room_id INTEGER(11),
+  text VARCHAR(1000),
+  username_id INTEGER(11)
+  -- FOREIGN KEY(username) REFERENCES (usernames.text),
+  -- FOREIGN KEY(room_id) REFERENCES (rooms(id))
+  /* Describe your table here.
+  message id
+  room_name
+  username
+  text
+  */
 );
 
+CREATE TABLE rooms (
+  id INTEGER(11) AUTO_INCREMENT PRIMARY KEY,
+  text VARCHAR(1000)
+);
+
+
+CREATE TABLE usernames (
+  id INTEGER(11) AUTO_INCREMENT PRIMARY KEY,
+  text VARCHAR(1000)
+);
 /* Create other tables and define schemas for them here! */
 
 
@@ -15,3 +38,6 @@ CREATE TABLE messages (
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+DESCRIBE messages;
+DESCRIBE rooms;
+DESCRIBE usernames;
